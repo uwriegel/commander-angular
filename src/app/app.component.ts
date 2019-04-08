761a8b979ba762c9e79a60dbaf774641a1279f92
uriegel <uriegel@hotmail.de>
2019-04-08 17:51:55 +0200

    Run addon in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+declare var process
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'commander';
+
+    constructor() {
+        if ((<any>window).require) {
+            try {
+                this.addon = (<any>window).require("extension-fs")
+            } catch (error) {
+                throw error
+            }
+        } else 
+            console.warn("Could not load electron ipc");             
+    }
+
+    async callAddon() {
+        let files = await this.addon.getFiles("c:/windows/system32")
+        //let files = await addon.getFiles("d:/Test😎😎😎")
+    
+        const hrstart = process.hrtime()
+        //files = await addon.getFiles("c:/windows/system32")
+        const diff = process.hrtime(hrstart)
+        console.info(`Execution time files (hr): ${(diff[1] / 1000000.0)}`)
+    }
+
+    private addon: any
+    private test: any 
 }
