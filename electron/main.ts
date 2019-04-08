3a84530c1197647e675154fb01105db2b18c67cc
uriegel <uriegel@hotmail.de>
2019-04-08 17:38:05 +0200

    Running angular in electron

---
+++ electron/main.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from "electron"
+import * as path from "path"
+import * as url from "url"
+
+let win: BrowserWindow
+
+const createWindow = function() {
+    win = new BrowserWindow({ 
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }        
+    })
+
+    console.log(process.cwd())
+
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/../../dist/commander/index.html`),
+            protocol: "file:",
+            slashes: true
+        })
+    )
+    
+    win.on("closed", () => win = null)
+}
+
+app.on("ready", createWindow)
+
+app.on("activate", () => {
+    if (win === null) 
+        createWindow()
+})
