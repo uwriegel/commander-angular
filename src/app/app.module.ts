068f85e4618756f2b42daace57397e85e84661a2
uriegel <uriegel@hotmail.de>
2019-04-11 20:42:10 +0200

    Eliminated datatemplates and createComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,11 @@ import { ColumnsComponent } from './columns/columns.component';
 import { VirtualListPipe } from './pipes/virtual-list.pipe';
 import { TestColumnsComponent } from './test/columns/columns.component';
 import { ScrollbarComponent as TestScrollbarComponent } from './test/scrollbar/scrollbar.component';
-import { TemplatesComponent } from './test/datatemplates/templates/templates.component';
-import { DataTemplateComponent } from './test/datatemplates/data-template/data-template.component';
-import { DataTemplate2Component } from './test/datatemplates/data-template2/data-template2.component';
 import { TableViewComponent } from './table-view/table-view.component';
 import { ClipHeightPipe } from './pipes/clip-height.pipe';
 import { TableViewItemComponent } from './table-view-item/table-view-item.component';
 import { FolderComponent } from './icon/folder/folder.component';
 import { DriveComponent } from './icon/drive/drive.component';
-import { TableViewComponent as TestTableViewComponent } from './test/table-view/table-view/table-view.component';
-import { ListItemDirective } from './test/table-view/directives/list-item.directive';
-import { ItemTemplateComponent } from './test/table-view/item-template/item-template.component';
-import { TableViewContainerComponent } from './test/table-view/table-view-container/table-view-container.component';
 
 @NgModule({
     declarations: [
@@ -33,18 +26,11 @@ import { TableViewContainerComponent } from './test/table-view/table-view-contai
         VirtualListPipe,
         TestColumnsComponent,
         TestScrollbarComponent,
-        TemplatesComponent,
-        DataTemplateComponent,
-        DataTemplate2Component,
         TableViewComponent,
         ClipHeightPipe,
         TableViewItemComponent,
         FolderComponent,
         DriveComponent,
-        TestTableViewComponent,
-        ListItemDirective,
-        ItemTemplateComponent,
-        TableViewContainerComponent,
     ],
     imports: [
         BrowserModule,
@@ -52,8 +38,5 @@ import { TableViewContainerComponent } from './test/table-view/table-view-contai
     ],
     providers: [],
     bootstrap: [AppComponent],
-    entryComponents: [
-        ItemTemplateComponent
-    ]
 })
 export class AppModule { }
